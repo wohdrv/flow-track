5becfff0e9cf82ab270accfb1007954873df294e
Zhanibek Nurmukhambet <133672243+wohdrv@users.noreply.github.com>
2026-02-06 18:32:44 +0500

    nodemcu
    Refactor code to improve flood detection and WiFi connection messages.

--- nodemcu/nodemcu.ino
+++ nodemcu/nodemcu.ino
@@ -1,51 +1,49 @@
 #include <SoftwareSerial.h>
 #include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
 #include <UniversalTelegramBot.h>
 
 #define WIFI_SSID "ALHN-0895"
 #define WIFI_PASSWORD "3797255625"
-WiFiClientSecure secured_client;
-
 #define BOT_TOKEN "7681869452:AAGgitL7jTsZf_OgmZG-apAtKTKEMJrgg8w"
 #define CHAT_ID "5173361522"
-const unsigned long BOT_MTBS = 100;
+
+WiFiClientSecure secured_client;
 X509List cert(TELEGRAM_CERTIFICATE_ROOT);
 UniversalTelegramBot bot(BOT_TOKEN, secured_client);
-unsigned long bot_lasttime;
 
-SoftwareSerial s(D6, D5); // RX, TX
-int data;
+SoftwareSerial s(D6, D5);
+
+const int sensorPin = A0;
+bool isFlooded = false;
 
 void setup() {
-  //отладка
-  Serial.begin(9600); 
-
-  //связь с ардуино
+  Serial.begin(9600);
   s.begin(9600);
 
   configTime(0, 0, "pool.ntp.org");
   secured_client.setTrustAnchors(&cert);
-  Serial.print("Connecting to WiFi SSID...");
+
+  Serial.print("Connecting to WiFi...");
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
     delay(300);
   }
-  Serial.print("\nWiFi connected. IP adress: ");
-  Serial.print(WiFi.localIP());
-  bot.sendMessage(CHAT_ID, "Hi! I'm online.", "");
+  
+  Serial.println("\nWiFi connected.");
+  
+  bot.sendMessage(CHAT_ID, "Flow track activated.", "");
 }
 
 void loop() {
-  s.write("s");
-  bot.sendMessage(CHAT_ID, "Sent: s", "");
-
-  if (s.available() > 0) {
-    data = s.read();
-    if (data == 50) {
-      bot.sendMessage(CHAT_ID, "Recieved: 50", "");
-    }
+  int waterLevel = analogRead(sensorPin);
+  if (waterLevel > 400 && !isFlooded) {
+    Serial.println("Leak detected!");
+    bot.sendMessage(CHAT_ID, "Water detected!!!", "");
+    s.write('1'); 
+    delay(5000); 
+    bot.sendMessage(CHAT_ID, "Tap is closed.", "");
+    isFlooded = true; 
   }
-
-  delay(5000);
 }
